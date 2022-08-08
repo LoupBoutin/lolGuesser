ff88154e8a40ac731fb1521fa98251a5268d228c
LoupBoutin <101940517+LoupBoutin@users.noreply.github.com>
2022-08-09 00:52:06 +0200

    Add files via upload
    jeu bien fonctionnel avec couleurs pour les vbox

--- demo1/src/main/java/com/example/demo1/Controller.java
+++ demo1/src/main/java/com/example/demo1/Controller.java
@@ -5,17 +5,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
 import static com.example.demo1.Main.*;
 
@@ -23,28 +23,30 @@ public class Controller {
     @FXML
     ImageView myImageView = new ImageView("file:src\\main\\resources\\Image\\beemo.png");
     @FXML
-    Label myLabel;
+    Text myText;
     @FXML
     TextField myTextField;
     @FXML
     public Button myButton;
-
     @FXML
-    Text nameText;
+    Text nameOfActualChampText;
     @FXML
-    Text genderText;
+    Text genderOfActualChampText;
     @FXML
-    Text positionText;
-
-    @FXML
-    VBox nameBox;
-    @FXML
-    VBox genderBox;
-    @FXML
-    VBox positionBox;
-
+    Text positionOfActualChampText;
+    @FXML Text nameText;
+    @FXML Text genderText;
+    @FXML Text positionText;
+    @FXML VBox nameBoxOfAC;
+    @FXML VBox genderBoxOfAC;
+    @FXML VBox positionBoxOfAC;
+    @FXML VBox nameBox;
+    @FXML VBox genderBox;
+    @FXML VBox positionBox;
     String sc;
     String[] similitude = {"NAME", "GENDER", "POSITION"};
+    String[] actualChampChar = {"NAME", "GENDER", "POSITION"};
+    Champion guessTheChamp;
 
     /**
      * Renvoie un String. -> input de myTextField.
@@ -55,134 +57,79 @@ public class Controller {
      */
     @FXML
     public void handle(KeyEvent event) throws FileNotFoundException {
+
+        ArrayList<Champion> championsReadFromFile; // la liste des champions sous forme de reference.
+        championsReadFromFile = readChampFromFile("src\\main\\java\\com\\example\\demo1\\champText.txt"); // lit le text qui donne la liste de champions.
+        ArrayList<String> champNameList = champNameListString(championsReadFromFile); // la liste des noms de champions.
+        if (guessTheChamp == null) {
+            guessTheChamp = getRandomItem(championsReadFromFile);
+        }
+
         if (event.getCode() == KeyCode.ENTER) {
             sc = myTextField.getText();
             System.out.println(sc);
-            ArrayList<Champion> championsReadFromFile; // la liste des champions sous forme de reference.
-            championsReadFromFile = readChampFromFile("src\\main\\java\\com\\example\\demo1\\champText.txt"); // lit le text qui donne la liste de champions.
-            ArrayList<String> champNameList = champNameListString(championsReadFromFile); // la liste des noms de champions.
+
             sc = myTextField.getText();
             if (champNameList.contains(sc)) {
                 System.out.println("c'est valide");
+                myText.setText(null);
                 Champion actualChampion = scFindObject(sc, championsReadFromFile); // renvoie le champion qui était dans myTextField.
-                Champion guessChampion = getRandomItem(championsReadFromFile);
+                nameOfActualChampText.setText(actualChampion.getName());
+                genderOfActualChampText.setText(actualChampion.getGender());
+                positionOfActualChampText.setText(actualChampion.getPosition());
 
-                if (actualChampion.getName().equals(guessChampion.getName())) {
+                if (actualChampion.getName().equals(guessTheChamp.getName())) {
                     System.out.println("Name : Correct");
                     similitude[0] = (actualChampion.getName());
                     nameText.setText(actualChampion.getName());
+                    nameBoxOfAC.setBackground(Background.fill(Color.GREEN));
+                    nameBox.setBackground(Background.fill(Color.GREEN));
                 } else {
                     System.out.println("Name : Incorrect");
+                    nameBoxOfAC.setBackground(Background.fill(Color.RED));
                 }
 
-                if (actualChampion.getGender().equals(guessChampion.getGender())) {
+                if (actualChampion.getGender().equals(guessTheChamp.getGender())) {
                     System.out.println("Gender : Correct");
                     similitude[1] = (actualChampion.getGender());
                     genderText.setText(actualChampion.getGender());
+                    genderBoxOfAC.setBackground(Background.fill(Color.GREEN));
+                    genderBox.setBackground(Background.fill(Color.GREEN));
                 } else {
                     System.out.println("Gender : Incorrect");
+                    genderBoxOfAC.setBackground(Background.fill(Color.RED));
                 }
 
-                if (actualChampion.getPosition().equals(guessChampion.getPosition())) {
+                if (actualChampion.getPosition().equals(guessTheChamp.getPosition())) {
                     System.out.println("Position : Correct");
                     similitude[2] = (actualChampion.getPosition());
                     positionText.setText(actualChampion.getPosition());
+                    positionBoxOfAC.setBackground(Background.fill(Color.GREEN));
+                    positionBox.setBackground(Background.fill(Color.GREEN));
                 } else {
                     System.out.println("Position : Incorrect");
+                    positionBoxOfAC.setBackground(Background.fill(Color.RED));
                 }
-
-                System.out.println(Arrays.toString(similitude));
-
-            } else { System.out.println("c'est pas valide"); }
+            } else {
+                System.out.println("c'est pas valide");
+                myText.setText("Invalid champion name");
+            }
         }
     }
     @FXML
-    public void submit(ActionEvent event) {
-        try {
-            ArrayList<Champion> championsReadFromFile; // la liste des champions sous forme de reference.
-            championsReadFromFile = readChampFromFile("src\\main\\java\\com\\example\\demo1\\champText.txt"); // lit le text qui donne la liste de champions.
-            ArrayList<String> champNameList = champNameListString(championsReadFromFile); // la liste des noms de champions.
-            sc = myTextField.getText();
-            if (champNameList.contains(sc)) {
-                System.out.println("c'est valide");
-                Champion actualChampion = scFindObject(sc, championsReadFromFile); // renvoie le champion qui était dans myTextField.
-                Champion guessChampion = getRandomItem(championsReadFromFile);
+    public void submit(ActionEvent event) throws FileNotFoundException {
+        ArrayList<Champion> championsReadFromFile; // la liste des champions sous forme de reference.
+        championsReadFromFile = readChampFromFile("src\\main\\java\\com\\example\\demo1\\champText.txt"); // lit le text qui donne la liste de champions.
+        ArrayList<String> champNameList = champNameListString(championsReadFromFile); // la liste des noms de champions.
+        guessTheChamp = getRandomItem(championsReadFromFile);
 
-                    if (actualChampion.getGender().equals(guessChampion.getGender())) {
-                        System.out.println("Gender : Correct");
-                        similitude[2] = (actualChampion.getGender());
-                    } else {
-                        System.out.println("Gender : Incorrect");
-                    }
-
-                    if (actualChampion.getPosition().equals(guessChampion.getPosition())) {
-                        System.out.println("Position : Correct");
-                    } else {
-                        System.out.println("Position : Incorrect");
-                    }
-
-
-            } else { System.out.println("c'est pas valide"); }
-
-        } catch (Exception e) { System.out.println(e); }
+        nameText.setText("NAME");
+        nameBox.setBackground(Background.fill(Color.BLACK));
+        genderText.setText("GENDER");
+        genderBox.setBackground(Background.fill(Color.BLACK));
+        positionText.setText("POSITION");
+        positionBox.setBackground(Background.fill(Color.BLACK));
+        Image myImage = new Image(getClass().getResourceAsStream("file:src\\main\\resources\\Image\\beemo.png"));
+        myImageView.setImage(myImage);
     }
-
-
-            /*System.out.println(champNameList);
-            //System.out.println(championsReadFromFile);
-            try {
-
-                Champion actualChampion = scFindObject(isInTheList(sc, champNameList), championsReadFromFile);
-                if (!(champNameList.contains(sc))) {
-                    myTextField.setPromptText("Type a valid champion name... ");
-                }
-                System.out.println(actualChampion);
-            } catch (Exception e) {
-                System.out.println("error frerot");
-            }
-
-
-
-
-
-        /*Champion aatrox = championsReadFromFile.get(0);
-        Champion ahri = championsReadFromFile.get(1);
-        Champion akali = championsReadFromFile.get(2);
-        Champion akshan = championsReadFromFile.get(3);*/
-
-        /*Champion actualChampion = scFindObject(isInTheList( champNameList), championsReadFromFile); // ici isInTheList fait appel au shell pour justement scanner un nom et voir
-        // si il est dans la liste des noms de champions, ensuite scFindObject transforme le String scanner en object.
-        Champion guessChampion = getRandomItem(championsReadFromFile);
-
-        while (!winCondition(actualChampion, guessChampion)) {
-
-            if (actualChampion.getGender().equals(guessChampion.getGender())) {
-                System.out.println("Gender : Correct");
-                similitude[2] = (actualChampion.getGender());
-            } else {
-                System.out.println("Gender : Incorrect");
-            }
-
-            if (actualChampion.getPosition().equals(guessChampion.getPosition())) {
-                System.out.println("Position : Correct");
-            } else {
-                System.out.println("Position : Incorrect");
-            }
-            actualChampion = scFindObject(isInTheList( champNameList), championsReadFromFile);
-        }*/
-    /**
-     * Renvoie un String. -> si le scanner est bien dans la liste, alors on revoie le String scanner.
-     * Compare le scanner ( variable locale à la fonction ) et l'arrayList prit en paramètre.
-     * Prend en paramètre:
-     * ArrayList de String the List -> l'arrayList de champNameListString(ArraList Champion theList).
-     * @param sc
-     * @param theList
-     * @return
-    */
-   /*public static String isInTheList(String sc, ArrayList<String> theList) {
-        if (!(theList.contains(sc))) {
-            myTextField.setText("Type a valid champion name... ");
-        }
-        return sc;
-    }*/
 }
--- demo1/src/main/java/com/example/demo1/Main.java
+++ demo1/src/main/java/com/example/demo1/Main.java
@@ -1,17 +1,12 @@
 package com.example.demo1;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -23,7 +18,6 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("myDisplay.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-
 
         Image iconImage = new Image("file:src\\main\\resources\\Image\\beemo.png");
         stage.getIcons().add(iconImage);
